4ecf712079b534a386507d72aa5770ac963acc17
Syize <syizeliu@gmail.com>
2026-09-12 10:10:44 +0800

    Update extension permissions

--- manifest.config.ts
+++ manifest.config.ts
@@ -2,32 +2,27 @@ import { defineManifest } from '@crxjs/vite-plugin'
 import pkg from './package.json'
 
 export default defineManifest({
-  manifest_version: 3,
-  name: pkg.name,
-  version: pkg.version,
-  icons: {
-    48: 'public/logo.png',
-  },
-  action: {
-    default_icon: {
-      48: 'public/logo.png',
-    },
-    default_popup: 'src/popup/index.html',
-  },
-  permissions: [
-    'sidePanel',
-    'contentSettings',
-    'activeTab',
-  ],
-  host_permissions: [
-    'http://localhost:23119/*',
-    'http://127.0.0.1:23119/*',
-  ],
-  content_scripts: [{
-    js: ['src/content/main.tsx'],
-    matches: ['http://*/*', 'https://*/*'],
-  }],
-  side_panel: {
-    default_path: 'src/sidepanel/index.html',
-  },
+	manifest_version: 3,
+	name: pkg.name,
+	version: pkg.version,
+	icons: {
+		48: 'public/logo.png',
+	},
+	action: {
+		default_icon: {
+			48: 'public/logo.png',
+		},
+		default_popup: 'src/popup/index.html',
+	},
+	permissions: ['sidePanel'],
+	host_permissions: ['http://localhost:23119/*', 'http://127.0.0.1:23119/*'],
+	content_scripts: [
+		{
+			js: ['src/content/main.tsx'],
+			matches: ['http://*/*', 'https://*/*'],
+		},
+	],
+	side_panel: {
+		default_path: 'src/sidepanel/index.html',
+	},
 })
